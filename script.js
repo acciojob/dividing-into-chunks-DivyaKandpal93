764f7664510f5dcb8daf2b8728d21088c4c6d320
Divya kandpal <80275853+DivyaKandpal93@users.noreply.github.com>
2024-02-18 15:33:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,26 +4,23 @@ let subArray = [];
 let sum = 0;
 
 const divide = (arr, n) => {
-  // Write your code here
-	arr.forEach(num => {
-		if(sum + num <= n){
-			subArray.push(num);
-			sum += num;
-			
-		}
-
-		else {
-			result.push(subArray);
-			subArray = [num];
-			sum = num;
-		}
-	})
-;
-if (subArrayrray.length > 0) {
-        result.push(subArray);
+  arr.forEach(num => {
+    if (sum + num <= n) {
+      subArray.push(num);
+      sum += num;
+    } else {
+      result.push(subArray);
+      subArray = [num];
+      sum = num;
     }
+  });
 
-    return result;
+  if (subArray.length > 0) {
+    result.push(subArray);
+  }
 
-const n = prompt("Enter n: ");
+  return result;
+}
+
+const n = parseInt(prompt("Enter n: "));
 alert(JSON.stringify(divide(arr, n)));
